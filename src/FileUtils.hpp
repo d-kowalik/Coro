efd3bf81d807f4756b85c1ea1d05b112d2e1a764
Damian Kowalik <dev.damian.kowalik@gmail.com>
2019-09-09 21:08:03 +0200

    Add function to read files

---
+++ src/FileUtils.hpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <optional>
+#include <string>
+#include <vector>
+
+namespace pge::FileUtils {
+
+std::optional<std::string> Read(const std::string& path) {
+    std::ifstream in(path);
+    if (in.is_open()) {
+        in.unsetf(std::ios::skipws);
+        std::string result{};
+        std::copy(std::istream_iterator<char>(in),
+                  std::istream_iterator<char>(), std::back_inserter(result));
+
+        return result;
+    }
+    return {};
+}
+
+}  // namespace pge::FileUtils
