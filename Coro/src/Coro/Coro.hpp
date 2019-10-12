59d92a42a7941b9f8cca56e089cb13e453a64840
Damian Kowalik <dev.damian.kowalik@gmail.com>
2019-10-13 00:33:16 +0200

    Port the project to Linux and use CMake

---
+++ Coro/src/Coro/Coro.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "Application.hpp"
+#include "Input/Input.hpp"
+#include "Input/Keycodes.hpp"
--- Sandbox/src/main.cpp
+++ Sandbox/src/main.cpp
@@ -1,8 +1,4 @@
-#include <iostream>
-#include <random>
-
-#include <Coro/Application.hpp>
-#include<Coro/Input/Input.hpp>
+#include <Coro/Coro.hpp>
 
 constexpr int W = 1280;
 constexpr int H = 720;
@@ -10,45 +6,152 @@ constexpr char TITLE[] = "Coro";
 
 class SandboxApp : public Coro::Application
 {
-	using Application::Application;
+public:
+    using Application::Application;
 
-	std::random_device _rd;
-	std::mt19937 _gen{ _rd() };
-	std::uniform_real_distribution<float> _dist{ 0.f, 1.f };
+private:
+    static const int GRID_X = 10;
+    static const int GRID_Y = 24;
+    const float _pixelSize = static_cast<float>(H) / GRID_Y; // at 720p should be 30.f
+    const float _pixelsX = W / _pixelSize;
+    const float _pixelsY = H / _pixelSize;
 
-	bool OnUserCreate() override {
-		return true;
-	};
+    const float _offsetX = (_pixelsX - GRID_X) / 2 * _pixelSize;
+    char _board[GRID_X][GRID_Y] = {'#'};
 
-	bool OnUserUpdate(float delta) override {
-		if (Coro::Input::IsKeyPressed(Coro::Key::ESCAPE)) {
-			Quit();
-		}
+    float _dropCooldown = 0.f;
+    float _moveCooldown = 0.f;
 
-		static const char _map[] = "########"
-			"#..##..#"
-			"#..##..#"
-			"########"
-			"#.####.#"
-			"##....##"
-			"########"
-			"########";
+    void SpawnL() {
+        const int top = GRID_Y - 1;
+        const int mid = GRID_X / 2;
 
-		static const float pixelSize = H / 8.f;
-		for (int x = 0; x < 8; x++) {
-			for (int y = 0; y < 8; y++) {
-				DrawPixel(pixelSize, pixelSize, x * pixelSize, y * pixelSize, _map[8 * y + x] == '.' ? 0 : 1, .5f, .5f);
-			}
-		}
+        _board[mid][top] = '.';
+        _board[mid][top - 1] = '.';
+        _board[mid][top - 2] = '.';
+        _board[mid + 1][top - 2] = '.';
+    }
 
-		return true;
-	};
+    void SpawnT() {
+        const int top = GRID_Y - 1;
+        const int mid = GRID_X / 2;
+
+        _board[mid][top] = '.';
+        _board[mid][top - 1] = '.';
+        _board[mid - 1][top - 1] = '.';
+        _board[mid + 1][top - 1] = '.';
+    }
+
+    void SpawnI() {
+        const int top = GRID_Y - 1;
+        const int mid = GRID_X / 2;
+
+        _board[mid][top] = '.';
+        _board[mid][top - 1] = '.';
+        _board[mid][top - 2] = '.';
+        _board[mid][top - 3] = '.';
+    }
+
+    void DropDown() {
+        bool transformToStatic = false;
+        for (int y = 0; y < GRID_Y; y++) {
+            for (auto& x : _board) {
+                if (x[y] == '.') {
+                    if (y == 0 || x[y - 1] == 'S') {
+                        transformToStatic = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        for (int y = 0; y < GRID_Y; y++) {
+            for (auto& x : _board) {
+                if (x[y] == '.') {
+                    if (transformToStatic) {
+                        x[y] = 'S';
+                    }
+                    else {
+                        x[y] = '#';
+                        x[y - 1] = '.';
+                    }
+                }
+            }
+        }
+    }
+
+    void MoveLeft() {
+        if (_moveCooldown < .1f) return;
+        _moveCooldown = 0.f;
+        for (int x = 0; x < GRID_X; x++) {
+            for (int y = 0; y < GRID_Y; y++) {
+                if (_board[x][y] == '.' ) {
+                    if (x == 0 || _board[x - 1][y] == 'S') return;
+                    _board[x][y] = '#';
+                    _board[x-1][y] = '.';
+                }
+            }
+        }
+    }
+
+    void MoveRight() {
+        if (_moveCooldown < .1f) return;
+        _moveCooldown = 0.f;
+        const int right = GRID_X - 1;
+        for (int x = right; x >= 0; x--) {
+            for (int y = 0; y < GRID_Y; y++) {
+                if (_board[x][y] == '.') {
+                    if (x == right || _board[x+1][y] == 'S') return;
+                    _board[x][y] = '#';
+                    _board[x + 1][y] = '.';
+                }
+            }
+        }
+    }
+
+    bool OnUserCreate() override {
+        std::fill(&_board[0][0], &_board[0][0] + sizeof(_board), '#');
+
+        return true;
+    };
+
+    bool OnUserUpdate(float delta) override {
+        if (Coro::Input::IsKeyPressed(Coro::Key::ESCAPE)) Quit();
+        if (Coro::Input::IsKeyPressed(Coro::Key::T)) SpawnT();
+        if (Coro::Input::IsKeyPressed(Coro::Key::I)) SpawnI();
+        if (Coro::Input::IsKeyPressed(Coro::Key::L)) SpawnL();
+        if (Coro::Input::IsKeyPressed(Coro::Key::RIGHT)) MoveRight();
+        if (Coro::Input::IsKeyPressed(Coro::Key::LEFT)) MoveLeft();
+        if (Coro::Input::IsKeyPressed(Coro::Key::DOWN)) _dropCooldown += .25f;
+
+        _dropCooldown += delta;
+        _moveCooldown += delta;
+        if (_dropCooldown >= 1.f) {
+            _dropCooldown = 0.f;
+            DropDown();
+        }
+
+        for (int x = 0; x < GRID_X; x++) {
+            for (int y = 0; y < GRID_Y; y++) {
+                if (_board[x][y] == '#') {
+                    DrawPixel(_pixelSize, _pixelSize, _pixelSize * x + _offsetX, _pixelSize * y, .5f, .5f, .5f);
+                } else if (_board[x][y] == '.') {
+                    DrawPixel(_pixelSize, _pixelSize, _pixelSize * x + _offsetX, _pixelSize * y, .1f, .7f, .9f);
+                }
+                else if (_board[x][y] == 'S') {
+                    DrawPixel(_pixelSize, _pixelSize, _pixelSize * x + _offsetX, _pixelSize * y, .1f, .9f, .4f);
+                }
+            }
+        }
+
+        return true;
+    };
 
 };
 
 int main() {
-	SandboxApp app{ TITLE, W, H };
-	app.Run();
+    SandboxApp app{ TITLE, W, H };
+    app.Run();
 
-	return 0;
+    return 0;
 }
